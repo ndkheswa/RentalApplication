32fe8268bf8112cacde30729322669dd6c962f2c
Ndoda Kheswa <daniel.kheswa@barclays.com>
2016-09-22 20:54:03 +0200

    Initial Commit

---
+++ RentalApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RentalApplication
+//
+//  Created by Ndoda Kheswa on 2016/09/22.
+//  Copyright © 2016 Simple-software Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
